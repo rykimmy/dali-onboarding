7e9daa5e3dcdfd12d2192e5204657fe7d407549b
Ryan <rkim20@choate.edu>
2023-09-26 17:59:10 -0400

    started react section

---
+++ react/src/index.jsx
@@ -0,0 +1,6 @@
+import './style.scss';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('main'));
+root.render(<Home />);
